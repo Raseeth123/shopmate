ebc7d4d1e51c40a8ba5169953880ce392a22da33
Raseeth123 <raseethansar@gmail.com>
2024-09-01 14:53:33 +0530

    Project:Shopmate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+import './index.css'
 import './App.css';
+import { Header } from './components/Header';
+import { Routes,Route } from 'react-router-dom';
+import { Home } from './components/Home';
+import { Cart } from './components/Cart';
+import { Footer } from './components/Footer';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const[productDetails,setproductDetails]=useState(JSON.parse(localStorage.getItem("Products")) || [{name:'Sony Wh-Ch510 Bluetooth Wireless',price:'$149',id:1,img:'https://shopmatecr-ul.netlify.app/assets/images/1001.png',cart:true},
+    {name:'boAt Rockerz 450',price:'$49',id:2,img:'https://shopmatecr-ul.netlify.app/assets/images/1002.png',cart:true},
+    {name:'JBL Tune 760NC',price:'$179',id:3,img:'https://shopmatecr-ul.netlify.app/assets/images/1003.png',cart:true},
+    {name:'Logitech H111 Wired',price:'$39',id:4,img:'https://shopmatecr-ul.netlify.app/assets/images/1004.png',cart:true},
+    {name:'APPLE Airpods Max Bluetooth Headset',price:'$199',id:5,img:'https://shopmatecr-ul.netlify.app/assets/images/1005.png',cart:true},
+    {name:'ZEBRONICS Zeb-Thunder Wired',price:'$29',id:6,img:'https://shopmatecr-ul.netlify.app/assets/images/1006.png',cart:true}])
+  const [count, setCount] = useState(() => {
+    const savedCount = localStorage.getItem('Count'); 
+    if (savedCount) {
+      try {
+        return JSON.parse(savedCount); 
+      } catch (error) {
+        console.error("Failed to parse count from localStorage:", error);
+        localStorage.removeItem('Count'); 
+      }
+    }
+    return 0; 
+  });
+  useEffect(() => {
+    localStorage.setItem("Count", JSON.stringify(count)); 
+  }, [count]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+      <Header count={count} setCount={setCount}/>
+      <div className='dark:bg-darkbg'>
+      <main>
+      <Routes>
+          <Route path="shop/cart" element={<Cart productDetails={productDetails} setproductDetails={setproductDetails}/>}></Route>
+          <Route path="*" element={<Home count={count} setCount={setCount} productDetails={productDetails} setproductDetails={setproductDetails}/>}></Route>
+      </Routes>
+      </main>
+      <Footer/>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react'
+
+export const Home = ({count,setCount,productDetails,setproductDetails}) => {
+ 
+    
+    const togglecart = (id) => {
+      setproductDetails(productDetails.map((prod) => {
+        if (prod.id === id) {
+          if (prod.cart) {
+            setCount(count + 1); 
+          } else {
+            setCount(count - 1); 
+          }
+          return { ...prod, cart: !prod.cart };
+        }
+        return prod;
+      }));
+    };
+
+    useEffect(()=>{
+      localStorage.setItem("Products",JSON.stringify(productDetails))
+    },[productDetails])
+    
+  return (
+    <>
+      <div className="flex justify-evenly flex-wrap mt-3">
+        {productDetails.map((prod) => (
+          <div key={prod.id} className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-3">
+            <img className="p-4 rounded-3xl h-64 w-full" src={prod.img} alt="product" />
+            <div className="px-5 pb-5">
+              <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{prod.name}</h5>
+              <div className="flex items-center mt-4 mb-5">
+                <div className="flex items-center space-x-1 rtl:space-x-reverse">
+                    <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                    </svg>
+                    <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                    </svg>
+                    <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                    </svg>
+                    <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                    </svg>
+                    <svg className="w-4 h-4 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                    </svg>
+                </div>
+                <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">5.0</span>
+                </div>
+              <div className="flex items-center justify-between mt-7">
+                <span className="text-xl font-semibold text-gray-900 dark:text-white">{prod.price}</span>
+                { (prod.cart) ? <button  onClick={()=>togglecart(prod.id)} className="text-white bg-blue-700 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600">Add to cart</button>:<button  onClick={()=>togglecart(prod.id)} className="text-white font-medium  bg-red-600 rounded-lg text-sm px-4 py-2 text-center dark:bg-red-600">Remove Item</button>}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
